bc444191fdcf94a2f6c6a32317170f6037d6f052
Daniel Altunay <daniel.altunay@flex.ai>
2025-05-13 17:02:12 +0200

    fix: upscale image using real-ersgan

--- scripts/resize_icons.sh
+++ scripts/resize_icons.sh
@@ -2,9 +2,9 @@
 
 cd extension/assets/icons
 
-magick icon.png -resize 16x16 icon16.png
-magick icon.png -resize 32x32 icon32.png
-magick icon.png -resize 48x48 icon48.png
-magick icon.png -resize 128x128 icon128.png
+convert icon.png -resize 16x16 icon16.png
+convert icon.png -resize 32x32 icon32.png
+convert icon.png -resize 48x48 icon48.png
+convert icon.png -resize 128x128 icon128.png
 
 echo "Icons resized successfully!"
